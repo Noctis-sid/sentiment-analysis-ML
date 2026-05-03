04c813427f3a24e64c53a63d03e805069f5cf80e
Sidhant Pujari <pujarisidhant@gmail.com>
2026-05-03 16:29:19 +0800

    Updated app run configuration

--- app.py
+++ app.py
@@ -93,5 +93,5 @@ def predict():
         'is_positive': pred == 'Good'
     })
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
